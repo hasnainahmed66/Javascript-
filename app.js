02cc607a8bfc93b2b236cd21a8fb10c13d27d0b7
hasnain <tracking.digiown@gmail.com>
2025-10-17 22:06:17 +0500

    javascript assigment

---
+++ app.js
@@ -0,0 +1,23 @@
+
+// Incrementing the value of number by 1
+
+
+let number = 5;
+number++;
+console.log("The number is: " + number);
+
+
+// Decrementing the value of number1 by 1
+
+let number1 = 10;
+number1--;
+console.log("The number is: " + number1);
+
+
+
+// let firstName = prompt("Enter your first name:");
+// let lastName = "Ahmed";
+// console.log("Full Name: " + firstName + " " + lastName);
+
+
+
