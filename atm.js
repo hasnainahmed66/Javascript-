5955499fe4db8f33d23c1a8cf9552ab2ab79eab2
hasnain <tracking.digiown@gmail.com>
2025-10-27 02:04:50 +0500

    updated something

---
+++ atm.js
@@ -0,0 +1,28 @@
+// let atmSalary = 25000;
+// let enteredAmount = prompt("Enter the amount you want to withdraw:");
+if (enteredAmount <= atmSalary) {
+    console.log("Please collect your cash: " + enteredAmount);
+    atmSalary -= enteredAmount;
+    console.log("Remaining ATM balance: " + atmSalary);
+}else {
+    console.log("Insufficient funds in ATM.");
+};
+
+
+alert("Hello User, Welcome to ATM");
+let pinCode = 1234;
+let enteredPin = prompt("Enter Your Pin Code:");
+let correctPin = 1234;
+let atmSalary = 25000;
+if (enteredPin === correctPin) {
+  alert("Pin Code is correct, You can proceed");
+}
+let enteredAmount = prompt("Enter the amount you want to withdraw:");
+if (enteredAmount <= atmSalary) {
+    console.log("Please collect your cash: " + enteredAmount);
+    atmSalary -= enteredAmount;
+    console.log("Remaining ATM balance: " + atmSalary);
+}else {
+    console.log("Insufficient funds in ATM.");
+};
+
---
+++ class.js
@@ -0,0 +1,20 @@
+ // gusses a number 
+let gusses = 10;
+ let num = prompt("Enter a number");
+
+if (num == 1){
+    console.log("correct");   
+}else if(num==2){
+    console.log("Bas ek step door ho");
+    
+}else if (num==3) {
+    console.log("thoda sa or try krlo");
+    
+}else if(num == 4){
+    console.log("");
+    
+}
+
+
+
+
