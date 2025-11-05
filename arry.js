99908fe1ae9951123cbabd5e709376d7bec8a80c
hasnain <tracking.digiown@gmail.com>
2025-11-05 09:45:33 +0500

    15 to 17

---
+++ arry.js
@@ -0,0 +1,28 @@
+let arr = [];
+arr.push("Hasnain");
+console.log(arr);
+
+
+
+let arr2 = new Array();
+arr.push("Hasnain");
+console.log(arr); 
+
+
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+console.log(fruits[0]); 
+console.log(fruits.length)
+
+
+let numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers.push("hasnain "));
+
+
+
+let mixedArray = ["Hasnain", 25, true, "JavaScript", false];
+
+
+console.log(mixedArray);
+
+
